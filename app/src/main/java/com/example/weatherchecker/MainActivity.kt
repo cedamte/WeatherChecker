f9b4d7b5e52560c70299be4d2bd6bf73c6e27156
Dami A <user@example.com>
2019-05-28 11:17:38 +0100

    Added dependencies in app module - explorartory test of Places API integration

--- app/src/main/java/com/example/weatherchecker/MainActivity.kt
+++ app/src/main/java/com/example/weatherchecker/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.weatherchecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Places.isInitialized().not()) Places.initialize(application, "AIzaSyB7g52X1Xu0cWPWFkCKa29ktRhOYqqzpHw")
+        val placesClient = Places.createClient(this)
+
+        val autocompleteSupportFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as
+                AutocompleteSupportFragment
+        autocompleteSupportFragment.setPlaceFields(listOf(Place.Field.NAME, Place.Field.ADDRESS_COMPONENTS))
+
+        autocompleteSupportFragment.setTypeFilter(TypeFilter.CITIES)
+        autocompleteSupportFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener{
+            override fun onPlaceSelected(place: Place) {
+                place.addressComponents
+            }
+
+            override fun onError(p0: Status) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
     }
 }
---
+++ app/src/main/java/com/example/weatherchecker/app/WeatherChecker.kt
@@ -0,0 +1,8 @@
+package com.example.weatherchecker.app
+
+import android.app.Application
+import dagger.android.HasActivityInjector
+
+class WeatherChecker : Application(){
+
+}
